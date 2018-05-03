96e6d3a330ffe4554e90d827f11b204589eafbbb
jjunxi <jjunxi417@gmail.com>
2018-05-03 13:11:51 -0400

    fix bug redirect to boss after bossinfo

--- web/client/src/container/bossinfo/bossinfo.js
+++ web/client/src/container/bossinfo/bossinfo.js
@@ -31,11 +31,11 @@ class BossInfo extends React.Component {
   }
 
   render() {
-    // const path = this.props.location.pathname; const redirect =
-    // this.props.redirectTo;
+    const path = this.props.location.pathname;
+    const redirect = this.props.redirectTo;
     return (
       <div>
-        {/* {redirect&&redirect!==path? <Redirect to={this.props.redirectTo}></Redirect> :null} */}
+        {redirect&&redirect!==path? <Redirect to={this.props.redirectTo}></Redirect> :null}
         <NavBar mode="dark">BOSS完善信息页</NavBar>
         <AvatarSelector
           selectAvatar={(imgname) => {
